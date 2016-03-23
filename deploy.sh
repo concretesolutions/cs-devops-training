63358ad7805b79081f3128d505a8633b54f2ae69
Travis CI <pedro.azevedo@concretesolutions.com.br>
2016-03-23 16:13:20 +0000

    Documentation updated at 8a58ad3

---
+++ deploy.sh
@@ -0,0 +1,22 @@
+#!/bin/bash -x
+
+rev=$(git rev-parse --short HEAD)
+
+cd _book
+
+git init
+git config user.name "Travis CI"
+git config user.email "pedro.azevedo@concretesolutions.com.br"
+
+git remote add upstream "https://${GH_TOKEN}@${GH_REF}" 
+git fetch upstream
+git reset upstream/gh-pages
+
+echo "www.concretesolutions.com.br/cs-devops-training" > CNAME
+
+touch .
+
+git add -A .
+git commit -m "Documentation updated at ${rev}"
+git push -q upstream HEAD:gh-pages
+
